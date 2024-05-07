758aad3bd28357ddf120a20246a9a822d63be294
annie-bakerc <annie.baker@uzh.ch>
2024-05-07 09:41:42 +0200

    monday_morning_df_update

--- scripts/data_import.R
+++ scripts/data_import.R
@@ -55,8 +55,3 @@ df_gdp <- wb_data("NY.GDP.PCAP.KD", start_date = 2020, end_date = 2020)
 df_lex <- wb_data("SP.DYN.LE00.IN", start_date = 2020, end_date = 2020)
 
 
-############## Making a dataframe from scratch ###########
-
-hello_df <- data.frame(column_1, column_2, column_3, column_4) #These are column names
-rm(hello_test)
-rm(hello_df)
--- scripts/df_scratch.R
+++ scripts/df_scratch.R
@@ -1,5 +1,8 @@
 ############## Making a dataframe from scratch ###########
 
-hello_df <- data.frame(column_1, column_2, column_3, column_4) #These are column names
-rm(hello_test)
+hello_df <- data.frame(column_1=1:5, 
+                       column_2=c('hi', 'hey', 'have', 'got', 'to'), 
+                       column_3 = c('there', 'hello', 'you', 'someplace', 'go?'))
+print(hello_df)
 rm(hello_df)
+
